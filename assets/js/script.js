71a898b056a3c6ac9ca2e6ed5891df3d01bf0c52
DaniloAS <daniloalvessantos4@gmail.com>
2020-09-24 04:05:22 -0300

    Adicionando JavaScript - Generation

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+
+// var - variavel de escopo global
+// let - variavel de escopo local
+// const - variavel de escopo global constante
+
+/*
+    JavaScript é Case Sensitive
+
+    formas de busca:
+    por Tag: getElementByTagName()
+    por Id: getElementById()
+    por Nome: getElementsByName()
+    por Classe: getElementByClassName()
+    por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 2){
+        txtNome.innerHTML = 'Nome Inválido!!'
+        txtNome.style.color = 'red'
+    }
+    else{
+        txtNome.innerHTML = 'Nome Válido!!'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'Email Inválido!!'
+        txtEmail.style.color = 'red'
+    }
+    else{
+        txtEmail.innerHTML = 'Email Válido!!'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto muito grande, Digite no máximo 100 Caracteres!!'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    }
+    else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert ('Formulario enviado com sucesso!!')
+    }
+    else{
+        alert ('Preencha o formulario corretamente...')
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.height = '300px'
+}
